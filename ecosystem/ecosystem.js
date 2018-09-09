54f496c5ccf726a0a26c1107beccc65a9310006a
Ben Patterson <bmizepatterson@gmail.com>
2018-09-08 22:11:38 -0400

    Make the fish swim around the screen

--- ecosystem/ecosystem.js
+++ ecosystem/ecosystem.js
@@ -1,18 +1,21 @@
-let movers = [];
+let school = [], c_water, c_fish;
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
-    for (let i = 0; i < 10; i++) {
-        movers[i] = new Mover();
+    c_water = color(50, 126, 206);
+    c_fish  = color(232, 109, 31);
+    let total = 1; //random(5,15);
+    for (let i = 0; i < total; i++) {
+        school[i] = new Fish(50, 25);
     }
 }
 
 function draw() {
-    clear();
-    for (let mover of movers) {
-        mover.update();
-        mover.checkEdges();
-        mover.display();
+    background(c_water);
+    for (let fish of school) {
+        fish.update();
+        fish.checkEdges();
+        fish.draw();
     }
 }
 
@@ -20,59 +23,105 @@ function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
 
-let Mover = function() {
-    this.factor       = Math.abs(randomGaussian(1, 0.2));
-    this.location     = createVector(random() * width, random() * height);
-    this.velocity     = createVector(0, 0);
-    this.acceleration; // = createVector(-0.01, 0.001);
-    this.topspeed     = 10 * this.factor;
-    this.r            = 50 * this.factor;
-    this.a            = 0;
-    this.b            = 10000;
+let Fish = function(w, h) {
+    const LEFT = -1;
+    const RIGHT = 1;
+    this.width = w;
+    this.height = h;
+    this.tankHeight = height;
+    this.tankWidth = width;
+    this.location = createVector(this.tankWidth/2, this.tankHeight/2);
+    this.prevLocation = this.location;
+    this.velocity = createVector(0, 0);
+    this.acceleration;
+    this.topspeed = 3;
+    this.a = 0;
+    this.b = 10000;
+    this.orientation;
 
     this.update = function() {
-        // Random acceleration
-        // this.acceleration = p5.Vector.random2D();
-        // this.acceleration.mult(random(2));
-
+        this.prevLocation = createVector(this.location.x, this.location.y);
         // Perlin noise acceleration
-        // this.acceleration = createVector(
-        //     map(noise(this.a), 0, 1, -1, 1), 
-        //     map(noise(this.b), 0, 1, -1, 1)
-        // );
+        // X-axis motion should be much greater than y-axis motion
 
-        // Acceleration towards the mouse
-        let mouse = createVector(mouseX, mouseY);
-        let dir = p5.Vector.sub(mouse, this.location);
-        let mag = dir.mag();
-        dir.normalize();
-        dir.mult(50/mag);
-        this.acceleration = dir;
-
+        this.acceleration = this.getAcceleration();
         this.velocity.add(this.acceleration);
         this.velocity.limit(this.topspeed);
         this.location.add(this.velocity);
+
+        // Determine orientation
+        if (this.prevLocation.x < this.location.x) {
+            this.orientation = RIGHT;
+        } else {
+            this.orientation = LEFT;
+        }
         this.a += 0.01;
         this.b += 0.01;
     }
 
-    this.display = function() {
-        stroke(255);
-        fill(255, 20);
-        ellipse(this.location.x, this.location.y, 2*this.r, 2*this.r);
+    this.getAcceleration = function() {
+        // Make copies of the velocity and position vectors for testing
+        let v = createVector(this.velocity.x, this.velocity.y);
+        let p = createVector(this.location.y, this.location.y);
+        let i = 0;
+        // fix this
+        while (true) {
+            let candidate = createVector(
+                map(noise(this.a), 0, 1, -1, 1), 
+                map(noise(this.b), 0, 1, -0.1, 0.1)
+            );
+            v.add(candidate);
+            p.add(v);
+            // Calculate the distance from the center horizontal that this 
+            // candidate vector would result in scored on on a scale of 0 - 1 
+            // (1 is edge of screen; 0 is on the center horizontal).
+            let yDist = Math.abs(this.tankHeight/2 - p.y) / 100;
+            console.log('yDist: ', yDist);
+            let r = random();
+            // The lower the yDist score, the higher the chance of this vector
+            // being returned.
+            if (r > yDist) {
+                return candidate;
+            }
+            return candidate;
+        }
+
+    }
+
+    this.draw = function() {
+        noStroke();
+        fill(c_fish);
+        translate(this.location.x, this.location.y);
+        if (this.orientation == RIGHT) {
+            rotate(PI);
+        }
+        ellipse(
+            0 - this.width * 0.1, 
+            0,
+            this.width * 0.8, 
+            this.height
+        );
+        triangle(
+            0,
+            0,
+            0 + this.width * 0.5,
+            0 - this.height * 0.5,
+            0 + this.width * 0.5,
+            0 + this.height * 0.5,
+        );
     }
 
     this.checkEdges = function() {
-        if (this.location.x > width) {
-          this.location.x = 0;
-        } else if (this.location.x < 0) {
-          this.location.x = width;
+        if (this.location.x + this.width/2 > this.tankWidth) {
+          this.location.x = this.prevLocation.x;
+        } else if (this.location.x - this.width/2 < 0) {
+          this.location.x = this.prevLocation.x;
         }
      
-        if (this.location.y > height) {
-          this.location.y = 0;
-        } else if (this.location.y < 0) {
-          this.location.y = height;
+        if (this.location.y + this.height/2 > this.tankHeight) {
+          this.location.y = this.prevLocation.y;
+        } else if (this.location.y - this.height/2 < 0) {
+          this.location.y = this.prevLocation.y;
         }
     }
 }
